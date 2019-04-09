fc4746b651baaca90de0d9c6f8801dc4f8a346c5
F3YoD <y.djama@hotmail.fr>
2019-04-09 16:10:40 +0200

    Problems with date conversion
    for orders date is in a form like this : XXX-XX-XXTXX:XX:XX-XX:XX

--- discogs_client/utils.py
+++ discogs_client/utils.py
@@ -13,7 +13,7 @@ except ImportError:
 
 def parse_timestamp(timestamp):
     """Convert an ISO 8601 timestamp into a datetime."""
-    return datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S')
+    return datetime.strptime(timestamp[:-6], '%Y-%m-%dT%H:%M:%S')
 
 
 def update_qs(url, params):
